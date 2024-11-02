2deca98ff4181a7d15e13e9e078446235c155c8e
ColtonDSnook <144720034+ColtonDSnook@users.noreply.github.com>
2024-11-02 15:53:25 -0300

    Finished implementing volume slider and options menu

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -10,12 +10,31 @@ public class SoundManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        if(!PlayerPrefs.HasKey("musicVolume"))
+        {
+            PlayerPrefs.SetFloat("musicVolume", 1);
+            Load();
+        }
 
+        else
+        {
+            Load();
+        }
     }
 
     public void ChangeVolume()
     {
         AudioListener.volume = volumeSlider.value; //adjusts the level of volume
 
+    }
+
+    private void Load()
+    {
+        volumeSlider.value = PlayerPrefs.GetFloat("musicVolume");
+    }
+
+    private void Save()
+    {
+        PlayerPrefs.SetFloat("musicVolume", volumeSlider.value);
     }
 }
